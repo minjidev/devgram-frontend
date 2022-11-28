7be2723de4da1e525932fb600a570cc659e3f75f
seongyeon12 <k.seongyeon12@gmail.com>
2022-11-28 23:54:30 +0900

    ADD Mypage

---
+++ src/pages/Mypage_Feed.jsx
@@ -0,0 +1,16 @@
+/* import MypageItems from '@components/mypage/mypage_items'
+import MypageProfile from '@components/mypage/mypage_profile' */
+
+/* export default function MypageFeed() {
+  return (
+  <div className='flex justify-between w-10/12 m-auto'>
+    <div className='w-3/12'> 
+    <MypageProfile />
+      </div>
+      <div className='w-8/12 bg-white'>
+        <MypageItems title="나의 피드" count="9" button="더보기 >" />
+    </div>
+    </div>
+  )
+}
+ */
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        port: 3000,
-    },
     resolve: {
         alias: {
             "@pages": path.resolve(__dirname, "./src/pages"),
