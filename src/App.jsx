6db905aab11863a191613121fc91f7d90d5ea70a
seongyeon12 <k.seongyeon12@gmail.com>
2022-12-01 23:59:58 +0900

    Update app.jsx to view mypage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Mypage from "./pages/Mypage";
+import MypageLike from "./pages/Mypage_like";
+import MypageMyFeed from "./pages/Mypage_MyFeed";
+import MypageUserFeed from "./pages/Mypage_Userfeed";
+import MypageFollowing from "./pages/Mypage_Following";
+import MypageMyReview from "./pages/Mypage_MyReview"
+
 function App() {
     return (
-        <div className="App">
-            <div className="bg-point-blue">테스트</div>
-        </div>
+        <>
+{/*             <MypageLike />
+            <MypageUserFeed /> */}
+            <Mypage />
+{/*             <MypageMyReview />
+            <MypageFollowing />
+            <MypageMyFeed /> */}
+        </>  
+        /* 밑코드 - 나중에 메인페이지로 연결할 것 */
+    // <BrowserRouter>
+    //     <Routes>
+    //         <Route path="/" element={<Mypage />}></Route>
+    //         {/* 에러사이트 처리
+    //         <Route path="*" element={<NotFound />}></Route> */}
+    //     </Routes>
+    // </BrowserRouter>
     );
 }
 
