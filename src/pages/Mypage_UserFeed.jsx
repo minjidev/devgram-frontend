43dd02d3c2c997be5908c08d86107e34dc3f33d9
seongyeon12 <k.seongyeon12@gmail.com>
2022-12-02 00:05:00 +0900

    Add Api connections

--- src/pages/Mypage_UserFeed.jsx
+++ src/pages/Mypage_UserFeed.jsx
@@ -1,14 +1,37 @@
-import MypageItems from '@components/mypage/mypage_items'
+import { useState, useEffect } from 'react'
+import MypageFeed from '@components/mypage/Mypage_Feed'
 import MypageProfile from '@components/mypage/mypage_profile'
+import MypageTitle from '@components/mypage/mypage_title'
 
-export default function MypageFeed() {
+export default function MypageUserFeed() {
+  const [feed, setFeed] = useState([]) 
+  // 나중에 대체 (상대 닉네임)
+  const userName = "이개발자"
+
+  useEffect(() => {
+		const fetchData = async() => {
+          const res = await fetch('https://a861831a-3d4e-42e0-9c51-7ca470881e0a.mock.pstmn.io/boards');
+          const result = res.json();
+          return result;
+        }	
+        
+        fetchData().then(res => setFeed(res));
+    }, []);
+
   return (
     <div className='flex justify-between w-10/12 m-auto'>
       <div className='w-3/12'> 
       <MypageProfile />
       </div>
       <div className='w-8/12 bg-white'>
-        <MypageItems title="나의 피드" count="9" button="더보기 >" />
+        <MypageTitle title={`${userName}의 피드`}></MypageTitle>
+        <ul className='flex justify-between flex-wrap gap-[1em]'>
+          {feed.map(f => (
+            <MypageFeed
+            feed={f.feed}
+            ></MypageFeed>
+          ))}
+        </ul>
       </div>
     </div>
   )
--- src/pages/Mypage_like.jsx
+++ src/pages/Mypage_like.jsx
@@ -1,14 +1,34 @@
-import MypageItems from '@components/mypage/mypage_items'
+import { useState, useEffect } from 'react'
+import MypageGoods from '@components/mypage/Mypage_goods'
 import MypageProfile from '@components/mypage/mypage_profile'
+import MypageTitle from '@components/mypage/Mypage_title'
 
 export default function MypageLike() {
+  const [like, setLike] = useState([])
+
+  useEffect(() => {
+		const fetchData = async() => {
+          const res = await fetch('https://fc0dd02c-41c7-4117-a17c-53569011aee5.mock.pstmn.io/product');
+          const result = res.json();
+          return result;
+        }	
+        
+        fetchData().then(res => setLike(res));
+    }, []);
+
   return (
     <div className='flex justify-between w-10/12 m-auto'>
       <div className='w-3/12'> 
       <MypageProfile />
       </div>
       <div className='w-8/12 bg-white'>
-        <MypageItems title="좋아요한 상품" count="9" button="더보기 >" />
+        <MypageTitle title="좋아요한 상품"/>
+        <ul className='flex justify-between flex-wrap gap-[1em]'>
+        {like.map(product => (
+          <MypageGoods title={product.title}
+          ></MypageGoods>
+        ))}
+        </ul>
       </div>
     </div>
   )
