954a1a8bfb25967a53b913e1ac8315f1cf978474
seongyeon12 <k.seongyeon12@gmail.com>
2022-12-09 20:44:47 +0900

    Add a router to connect mypage

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Mypage from "./pages/Mypage";
-import MypageLike from "./pages/Mypage_like";
-import MypageMyFeed from "./pages/Mypage_MyFeed";
-import MypageUserFeed from "./pages/Mypage_Userfeed";
-import MypageFollowing from "./pages/Mypage_Following";
-import MypageMyReview from "./pages/Mypage_MyReview"
+import { Router, Routes, Route, Link } from 'react-router-dom';
+/* import Example from "@mypage/responsive"; */
+import Mypage from "@mypage/Mypage";
+import MypageLike from "@mypage/Mypage_like";
+import MypageMyFeed from "@mypage/Mypage_MyFeed";
+import MypageUserFeed from "@mypage/Mypage_UserFeed";
+import MypageFollowing from "@mypage/Mypage_Following";
+import MypageMyReview from "@mypage/Mypage_MyReview"
+import MypageProfile from "@components/mypage/Mypage_profile"
+import styles from './index.css'
 
 function App() {
     return (
         <>
-{/*             <MypageLike />
-            <MypageUserFeed /> */}
-            <Mypage />
-{/*             <MypageMyReview />
-            <MypageFollowing />
-            <MypageMyFeed /> */}
-        </>  
-        /* 밑코드 - 나중에 메인페이지로 연결할 것 */
-    // <BrowserRouter>
-    //     <Routes>
-    //         <Route path="/" element={<Mypage />}></Route>
-    //         {/* 에러사이트 처리
-    //         <Route path="*" element={<NotFound />}></Route> */}
-    //     </Routes>
-    // </BrowserRouter>
+        <Routes>
+            <Route path="*" element={<Mypage />}></Route>
+            <Route path="/review" element={<MypageMyReview />}></Route>
+            <Route path="/like" element={<MypageLike />}></Route>
+            <Route path="/feed" element={<MypageMyFeed />}></Route>
+            <Route path="/userFeed" element={<MypageUserFeed />}></Route>
+            <Route path="/follow" element={<MypageFollowing />}></Route>
+        </Routes>
+    </>  
     );
 }
 
