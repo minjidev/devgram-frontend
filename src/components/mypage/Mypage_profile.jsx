d5a03a2aa2da34441f58b9465ea1f25236af041b
seongyeon12 <k.seongyeon12@gmail.com>
2022-12-09 21:04:35 +0900

    update tailwind css

--- src/components/mypage/Mypage_profile.jsx
+++ src/components/mypage/Mypage_profile.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import bird from "../../image/bird.jpg"
 
 export default function MypageProfile() {
   const [followerNum, setFollowerNum] = useState(0);
@@ -6,14 +7,16 @@ export default function MypageProfile() {
   const [likeNum, setLikeNum] = useState(0);
 /* after:contents-[""] after:pb-[100%] after:block */
   return (
-  <div className="w-full bg-blue-400 rounded-3xl h-80 text-center text-white">
-    <div className="img-box rounded-full bg-blue-600 w-28 h-28 m-auto"></div>
-    <h2 className='text-2xl mb-6 mt-3'>김개발자</h2>
-    <ul className='flex justify-center'>
-      <li>팔로워 {followerNum} |</li>
-      <li>팔로잉 {followNum} |</li>
-      <li>좋아요 {likeNum}</li>
-    </ul>
+  <div className="w-full bg-zinc-100 rounded-3xl h-80 text-center text-white drop-shadow mb-6">
+    <div className='pt-8 text-black'>
+      <img src={bird} className="drop-shadow img-box rounded-full bg-gray-500 w-40 h-40 m-auto mb-6"></img>
+        <h2 className='lg:text-2xl text-xl mb-3 mt-3 font-semibold'>김개발자</h2>
+        <ul className='flex justify-center'>
+          <li className='before:content-["|"] before:float-right before:block before:ml-1.5'>팔로워 <br />{followerNum}&nbsp;</li>
+          <li className='before:content-["|"] before:float-right before:block before:ml-1.5'>&nbsp;팔로잉 <br />{followNum}&nbsp;</li>
+          <li>&nbsp;좋아요 <br />{likeNum}</li>
+        </ul>
+      </div>
   </div>
   )
 }
