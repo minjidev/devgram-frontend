b060af6f233c77f864dd8b15547a2d26708dd82c
seongyeon12 <k.seongyeon12@gmail.com>
2022-12-09 21:03:28 +0900

    Add heroicon for star rating

--- src/components/mypage/Mypage_MainReview.jsx
+++ src/components/mypage/Mypage_MainReview.jsx
@@ -1,6 +1,7 @@
 import tw from "tailwind-styled-components"
+import { StarIcon } from '@heroicons/react/24/solid'
 
-export default function MainReview({key, title, star, review, name}) {
+export default function MainReview({title, star, review, name}) {
   const Item = tw.li`
   min-h-[150px]
   items-center
@@ -8,15 +9,37 @@ export default function MainReview({key, title, star, review, name}) {
   basis-[30%]
   grow
   `
-  // 폰트어썸 으로 할 것
-  const stars = "*****"
+
+  const stars = [
+    <StarIcon className="w-6 h-6 text-point-blue"></StarIcon>,
+    <StarIcon className="w-6 h-6 text-point-blue"></StarIcon>,
+    <StarIcon className="w-6 h-6 text-point-blue"></StarIcon>,
+    <StarIcon className="w-6 h-6 text-point-blue"></StarIcon>,
+    <StarIcon className="w-6 h-6 text-point-blue"></StarIcon>
+  ]
+
+  const num2 = stars.length - star
+  const num = stars.length - num2
+
+  if (num > 0) {
+    stars.splice(0, num2)
+    for (let i = 0; i < num2; i++) {
+      stars.push(<StarIcon className="iStar w-6 h-6 text-gray-200"/>)
+    }
+  }
 
   return (
-    <Item key={key}>
-      {title}
-      {name}
-      <div>{stars.substring(0, star)}</div>
-      {review.substring(0, 200)}
+    <>
+    <Item key={name}>
+      <h2 className="text-base font-medium">
+        {title}
+      </h2>
+      <p className="text-gray-500 after:content-[' '] after:block after:w-6 after:border-b-[1px] after:mt-6">{name}</p>
+      <div className="rating mt-3 mb-1.5 text-sm">
+          {stars}
+      </div>
+      <p>{review.length > 200 ? `${review.substring(0, 200)}···` : review}</p>
     </Item>
+    </>
   )
 }
--- src/components/mypage/Mypage_review.jsx
+++ src/components/mypage/Mypage_review.jsx
@@ -1,34 +1,61 @@
 import tw from "tailwind-styled-components"
+import { StarIcon } from '@heroicons/react/24/solid'
 
 export default function MypageReview({title, name, imgList = null, star, day, review}) {
   const Btn = tw.button`
-  w-24
-  h-12
   text-center
-  bg-blue-600
+  btn
   rounded-3xl
+  md:w-22 
+  text-white
+  md:btn-sm
+  hover:bg-point-blue
+  hover:border-point-blue
   `
+  
+  const stars = [
+    <StarIcon className="w-6 h-6 text-point-blue"></StarIcon>,
+    <StarIcon className="w-6 h-6 text-point-blue"></StarIcon>,
+    <StarIcon className="w-6 h-6 text-point-blue"></StarIcon>,
+    <StarIcon className="w-6 h-6 text-point-blue"></StarIcon>,
+    <StarIcon className="w-6 h-6 text-point-blue"></StarIcon>
+  ]
 
-  const stars = "*****"
+  const num2 = stars.length - star
+  const num = stars.length - num2
+
+  if (num > 0) {
+    stars.splice(0, num2)
+    for (let i = 0; i < num2; i++) {
+      stars.push(<StarIcon className="iStar w-6 h-6 text-gray-200"/>)
+    }
+  }
+
+  console.log(StarIcon)
 
   return (
     <>
-      <div>
-        <hr></hr>
+      <div className="mt-6">
         <div className="flex justify-between">
-          <h2>{title}</h2>
+            <div className="text-box">
+              <h2 className="text-base font-medium leading-8">{title}</h2>
+              <p className="text-gray-500">{name}</p>
+            </div>
           <div>
-            <Btn className="mr-12">수정</Btn>
+            <Btn className="mr-3">수정</Btn>
             <Btn>삭제</Btn>
           </div>
         </div>
-          <p>{name}</p>
           <div>{imgList}</div>
-          <div>{stars.substring(0, star)}{day}</div>
+          <div className="rating mt-6 mb-1.5 text-sm">
+          {stars}
+          <p className="ml-1.5 text-xs pt-[7px] text-gray-500">{day}</p>
       </div>
-      <p>
+      </div>
+      <p className="mb-3 mt-3 inline-block h-[50px] overflow-hidden">
         {review}
       </p>
+      <hr></hr>
     </>
   )
 }
