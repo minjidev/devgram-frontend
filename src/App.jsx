b24ceee9824f9b1a5e7f26ba43f81bc4fe6d5620
minji99710 <alswl99710@gmail.com>
2022-12-18 01:58:52 +0900

    Modify path

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Routes, Route } from "react-router-dom";
-import Search from "@components/MainPage/Header/Search";
-import MainPage from "@pages/Main/MainPage";
-import NotFound from "@components/MainPage/Main/NotFound";
+import Search from "@components/mainpage/header/Search";
+import MainPage from "@pages/main/MainPage";
+import NotFound from "@components/mainpage/main/NotFound";
 
 function App() {
     const client = new QueryClient({
@@ -19,7 +19,6 @@ function App() {
                         <Route path="/home" element={<MainPage />} />
                         {/* <Route path="/social/feed" element={<FeedPage />} /> */}
                         {/* <Route path="/login" element={<LogIn />} /> */}
-
                         {/* <Route path="/my" element={<Mypage />} /> */}
                         <Route path="/search" element={<Search />} />
                         <Route path="*" element={<NotFound />} />
