907c4ad2678404f3e5c72145342f86ebf4ec1d81
minji99710 <alswl99710@gmail.com>
2022-12-18 23:26:20 +0900

    Fix arrows in main carousel

--- src/components/mainpage/main/CarouselRanking.jsx
+++ src/components/mainpage/main/CarouselRanking.jsx
@@ -9,10 +9,10 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 function PrevBtn({ slickPrev }) {
     return (
         <button
-            className="slick-prev slick-arrow absolute top-1/2 -translate-y-2/4 -left-8 px-10"
+            className="absolute top-0 bottom-0 my-auto left-0 px-10"
             onClick={slickPrev}
         >
-            <ChevronLeftIcon className="w-6 h-6 sm:w-10 sm:h-10" />
+            <ChevronLeftIcon className="w-6 h-6 sm:w-10 sm:h-10 z-1" />
         </button>
     );
 }
@@ -20,7 +20,7 @@ function PrevBtn({ slickPrev }) {
 function NextBtn({ slickNext }) {
     return (
         <button
-            className="slick-next slick-arrow absolute bottom-1/2 translate-y-2/4 -right-8 px-10"
+            className="absolute top-0 bottom-0 my-auto px-10 right-0"
             onClick={slickNext}
         >
             <ChevronRightIcon className="w-6 h-6 sm:w-10 sm:h-10" />
@@ -58,7 +58,6 @@ function CarouselRanking() {
     if (isSuccess)
         return (
             <div className="relative">
-                <PrevBtn slickPrev={previous} />
                 <Slider ref={slider} {...settings}>
                     {data.map((d) => (
                         <div
@@ -84,6 +83,7 @@ function CarouselRanking() {
                         </div>
                     ))}
                 </Slider>
+                <PrevBtn slickPrev={previous} />
                 <NextBtn slickNext={next} />
             </div>
         );
