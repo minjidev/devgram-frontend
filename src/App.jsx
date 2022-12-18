658624d188f0f9e77182197ef98af2b7959ecb88
dongcheong <108535619+seongyeon12@users.noreply.github.com>
2022-12-19 02:45:25 +0900

    Update App.jsx
    fix path uppercase letters

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Routes, Route } from "react-router-dom";
-import Search from "@components/MainPage/Header/Search";
-import MainPage from "@pages/Main/MainPage";
-import NotFound from "@components/MainPage/Main/NotFound";
+import Search from "@components/mainpage/header/Search";
+import MainPage from "@pages/main/mainpage";
+import NotFound from "@components/mainpage/main/NotFound";
 import ProductsMainPage from "@pages/products/ProductsMainPage";
-import FeedWritePage from "@pages/Feed/Write/FeedWritePage";
+import FeedWritePage from "@pages/feed/write/FeedWritePage";
 
 function App() {
     const client = new QueryClient({
