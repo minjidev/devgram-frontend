71d097e7133af1aece1d8e4afab4a1b6f3fdedb5
seongyeon12 <k.seongyeon12@gmail.com>
2022-12-21 17:43:01 +0900

    remove unnecessary files

--- src/components/mypage/MyPageLink.jsx
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom"
-import tw from "tailwind-styled-components"
-
-export default function MypageLink({address}) {
-  return (
-    <Link to="/">Home</Link>
-  )
-}
