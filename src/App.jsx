ad8771854c2fe31189f86ef58b870dabdcebde17
seongyeon12 <k.seongyeon12@gmail.com>
2022-12-22 21:40:56 +0900

    Add url parameter(:id)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import AdminPage from "@pages/admin/AdminPage";
 import Search from "@components/mainpage/header/Search";
 import MainPage from "@pages/main/Mainpage";
@@ -22,6 +23,7 @@ function App() {
     const client = new QueryClient({
         defaultOptions: {},
     });
+    
     return (
         <>
             <div className="App">
@@ -29,10 +31,10 @@ function App() {
                     <Routes>
                         {/* <Route path="/" element={<MainPage />} /> */}
                         {/* <Route path="/social/feed" element={<FeedPage />} /> */}
-                        {/* <Route path="/login" element={<LogIn />} /> */}
+                        {/* <Route path="/login" element={(props) => <Login {...props} />} loginCallBack={loginCallBack} /> */}
 
                         {/* 마이페이지 */}
-                        <Route path="/my" element={<MyPage />} />
+                        <Route path="/my" element={<MyPage/>} />
                         <Route path="/my/review" element={<MyPageReview />} />
                         <Route path="/my/feed" element={<MyPageFeed />} />
                         <Route path="/my/like" element={<MyPageLike />} />
@@ -41,7 +43,7 @@ function App() {
 
                         <Route path="/products" element={<ProductsMainPage />}
                         />
-                        <Route path="/products/detail" element={<ProductsDatailPage />} />
+                        <Route path="/products/detail/:id" element={<ProductsDatailPage />} />
                         <Route path="/search" element={<Search />} />
                         <Route path="*" element={<NotFound />} />
                         <Route path="/social/feed/write" element={<FeedWritePage />} />
--- src/components/products/detail/ProductsDatailView.jsx
+++ src/components/products/detail/ProductsDatailView.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
 
 import Navigation from "@components/mainpage/header/Navigation"
 import ProductsDetailIntro from "@components/products/detail/ProductsDetailIntro"
@@ -6,48 +7,49 @@ import ProductsDetailPhoto from "@components/products/detail/ProductsDetailPhoto
 import ProductsDetailReview from "@components/products/detail/ProductsDetailReview"
 
 export default function ProductsDatailView() {
-  const [token, settoken] = useState([])
-  
-  useEffect(() => {
-  const fetchData = async() => {
-    const res = await fetch(`http://52.194.161.226:8080/api/user`);
-    const result = res.json();
-    return result;
-  }	
-  headers : {
-    Authorization : localStorage.getItem("access_token")
-  }
-  fetchData().then(res => settoken(res));
-  }, []);
+  const params = useParams();
+/*   const [products, setProducts] = useState([])
 
   useEffect(() => {
-  fetch("http://52.194.161.226:8080/api/user",{
-        method : "POST",
-        headers : {
-            Authorization : localStorage.getItem("access_token")
-        }
- })
-.then(response => response.json())
-.then(response => {
-   console.log(response.data);
-})
-}, []);
+		const fetchData = async() => {
+          const res = await fetch(`http://localhost:3001/product`);
+          const result = res.json();
+          return result;
+        }	
+        
+        fetchData().then(res => setProducts(res));
+    }, []);
+    console.log(products)
 
-  console.log(token)
+  let matchedItem = products.find(element => 
+      element.id === Number(params.id)
+    );
+    
+  console.log(matchedItem) */
 
   return (
-    <div className="m-auto text-black w-4/5 bg-white">
-      {/* nav */}
-      <Navigation />
+    <>
+    {/* nav */}
+    <Navigation />
 
-      {/* 상품 소개 */}
-      <ProductsDetailIntro />
+    <div className="m-auto text-black w-4/5 bg-white mt-6">
+
+      {/* 상품 소개 (버그 수정하고 주석 해제) */}
+{/*       <ProductsDetailIntro 
+      id={matchedItem.id}
+      title={matchedItem.title}
+      category_Seq={matchedItem.category_Seq}
+      rating={matchedItem.rating}
+      like_Count={matchedItem.like_Count}
+      price={matchedItem.price}
+      /> */}
 
       {/* 포토 칸 (리뷰 사진 리스트) */}
-      <ProductsDetailPhoto />
+      <ProductsDetailPhoto review={products} />
 
       {/* 리뷰 칸 */}
       <ProductsDetailReview />
     </div>
+    </>
   )
 }
