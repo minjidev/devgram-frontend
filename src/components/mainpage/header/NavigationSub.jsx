cde6488a56f936ea2dca18507c39f87e3e32ef62
minji99710 <alswl99710@gmail.com>
2022-12-24 00:13:55 +0900

    Fix a bug to show hr

--- src/components/mainpage/header/NavigationSub.jsx
+++ src/components/mainpage/header/NavigationSub.jsx
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar";
 
 function NavigationSub() {
     return (
-        <>
+        <div>
             <ul className="hidden sm:flex sm:flex-row items-center h-12 px-10 py-3 lg:px-32">
                 <li className="btn btn-xs px-3 btn-ghost text-xs rounded-full">
                     <button>
@@ -21,7 +21,7 @@ function NavigationSub() {
                 </li>
             </ul>
             <hr className="h-px bg-gray-100 border-0 dark:bg-gray-600" />
-        </>
+        </div>
     );
 }
 
